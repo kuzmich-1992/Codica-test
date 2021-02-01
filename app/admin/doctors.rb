6b5c65d834ccf0827b0d3bbaf563f0686b5c3ed2
Dima <kuzmichman92@gmail.com>
2021-02-01 22:59:31 +0200

    after add resurces ctive admin doctor\patient

---
+++ app/admin/doctors.rb
@@ -0,0 +1,2 @@
+ActiveAdmin.register Doctor do 
+end
--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -123,8 +123,8 @@ ActiveAdmin.setup do |config|
   #
   # This allows your users to comment on any resource registered with Active Admin.
   #
-  # You can completely disable comments:
-  # config.comments = false
+ 
+  config.comments = false
   #
   # You can change the name under which comments are registered:
   # config.comments_registration_name = 'AdminComment'
@@ -133,8 +133,8 @@ ActiveAdmin.setup do |config|
   # to be used for ordering:
   # config.comments_order = 'created_at ASC'
   #
-  # You can disable the menu item for the comments index page:
-  # config.comments_menu = false
+  
+  config.comments_menu = false
   #
   # You can customize the comment menu:
   # config.comments_menu = { parent: 'Admin', priority: 1 }

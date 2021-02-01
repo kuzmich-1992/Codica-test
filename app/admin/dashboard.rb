05a8b4abb1a0201a85d54e0a72809ef40f450a85
Dima <kuzmichman92@gmail.com>
2021-02-01 23:02:21 +0200

    after add resurces ctive admin doctor\patient2

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -8,13 +8,5 @@ ActiveAdmin.register_page "Dashboard" do
         small I18n.t("active_admin.dashboard_welcome.call_to_action")
       end
     end
-    
-    columns do
-      column do
-        panel "Info" do
-          para "Welcome to ActiveAdmin."
-        end
-      end
-    end
   end
 end

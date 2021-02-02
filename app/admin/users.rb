a15d97e85e68dcd2728a1a0267a4ad09fb650d0e
Dima <kuzmichman92@gmail.com>
2021-02-02 02:35:21 +0200

    after add loginas admin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,18 +1,3 @@
 ActiveAdmin.register User do
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :name, :multiple_role_id, :multiple_role_type
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :name, :multiple_role_id, :multiple_role_type]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
   
 end
